990c5a43b7aef3f1a1e0a2d80f32182c99ec2e15
Maciek P <mpikulicki@gmail.com>
2018-09-27 12:08:53 +0200

    Initial commit

---
+++ app/src/main/java/thesis/masters/registrationplates/MainActivity.java
@@ -0,0 +1,23 @@
+package thesis.masters.registrationplates;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug())
+        {
+            Toast.makeText(getApplicationContext(),"OpenCV is here", Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(getApplicationContext(),"Not loaded",Toast.LENGTH_SHORT).show();
+    }
+}
